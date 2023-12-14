6738bf16acb8f95f9a1d193506108c45561ac2b0
Mubinabd <153791173+Mubinabd@users.noreply.github.com>
2023-12-14 08:57:03 +0500

    exam number N1

---
+++ masala1.c
@@ -0,0 +1,24 @@
+#if 1
+//1-masala
+#include<stdio.h>
+int main(){
+    int n1;
+    int n2;
+    int n3;
+    int n4;
+    int num;
+
+    printf("number: ");scanf("%d",&num);
+
+    if(n1 = num){
+        printf("%d ",n1);
+    }if (n2 = num * num){
+        printf("%d ",n2);
+    }if(n3 = num *num *num){
+        printf("%d ",n3);
+    }if(n4 = num *num *num *num){
+        printf("%d ",n4);
+    }
+    return 0;
+}
+#endif
